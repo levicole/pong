a8d2931a09432cb3c2af4d9e6680933dbca78003
Levi Kennedy <lckennedy@gmail.com>
2015-08-11 16:33:21 -0500

    Little smoother motion.

--- pong/ViewController.swift
+++ pong/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     var ball     = UIView()
     var player1  = UIView()
     var divider  = UIView()
-    var panGusture = UIPanGestureRecognizer();
+    var panGusture = UIPanGestureRecognizer()
+    var ballDynamicBehavior = UIDynamicItemBehavior()
+    var paddleDynamicBehavior = UIDynamicItemBehavior()
+    var originalX = CGFloat()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,30 +26,40 @@ class ViewController: UIViewController {
         self.divider = UIView(frame: CGRectMake(0, self.view.frame.size.height/2, self.view.frame.size.width, 1))
         self.divider.backgroundColor = UIColor.darkGrayColor()
         self.view.addSubview(self.divider)
-        
+
         self.animator = UIDynamicAnimator(referenceView: self.view)
         self.ball     = UIView(frame: CGRectMake(100, 100, 10, 10))
         self.ball.backgroundColor = UIColor.redColor()
         self.view.addSubview(self.ball)
         
         self.panGusture = UIPanGestureRecognizer(target: self, action: "movePlayer:")
-        self.view.addGestureRecognizer(self.panGusture)
         
         
-        var player1Rect = CGRectMake(0, self.view.frame.height - 10, 80, 5)
+        var player1Rect = CGRectMake(0, self.view.frame.height - 20, 100, 15)
         self.player1 = UIView(frame: player1Rect)
         self.player1.backgroundColor = UIColor.darkGrayColor()
+        self.player1.addGestureRecognizer(self.panGusture)
         
         self.view.addSubview(self.player1)
         
-        var dynamicBehavior = UIDynamicItemBehavior(items: [self.ball])
-        var pushBehavior = UIPushBehavior(items: [self.ball], mode: UIPushBehaviorMode.Continuous)
+        self.ballDynamicBehavior = UIDynamicItemBehavior(items: [self.ball])
+        self.ballDynamicBehavior.allowsRotation = false
+        self.ballDynamicBehavior.friction   = 0.0
+        self.ballDynamicBehavior.elasticity = 1.0
+        self.ballDynamicBehavior.resistance = 0.0
+        animator.addBehavior(self.ballDynamicBehavior)
+        
+        self.paddleDynamicBehavior = UIDynamicItemBehavior(items: [self.player1])
+        self.paddleDynamicBehavior.allowsRotation = false
+        self.paddleDynamicBehavior.density = 1000
+        animator.addBehavior(self.paddleDynamicBehavior)
+        
+        var pushBehavior = UIPushBehavior(items: [self.ball], mode: UIPushBehaviorMode.Instantaneous)
         pushBehavior.setAngle(0.33, magnitude: 0.04)
         
         animator.addBehavior(pushBehavior)
-        animator.addBehavior(dynamicBehavior)
         
-        var collisionBehavior = UICollisionBehavior(items: [self.ball])
+        var collisionBehavior = UICollisionBehavior(items: [self.ball, self.player1])
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
         animator.addBehavior(collisionBehavior)
     }
@@ -57,8 +70,12 @@ class ViewController: UIViewController {
     }
 
     func movePlayer(sender: UIPanGestureRecognizer) {
-        var location = sender.locationInView(self.view)
-        self.player1.frame = CGRectMake(location.x, self.view.frame.height - 10, 80, 5)
+        if (sender.state == UIGestureRecognizerState.Began) {
+            self.originalX = self.player1.center.x
+        }
+        var translation  = sender.translationInView(self.view)
+        let newX = self.originalX + translation.x
+        self.player1.center = CGPointMake(newX, self.player1.center.y)
         animator.updateItemUsingCurrentState(self.player1)
     }
 
